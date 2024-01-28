3f454590870cb573aaebf1648df5ea3c5ced4f2a
Omer <sanli.omer@metu.edu.tr>
2024-01-28 23:48:31 +0300

    Initial Commit

---
+++ iTour-SwiftData/iTour_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTour_SwiftDataApp.swift
+//  iTour-SwiftData
+//
+//  Created by ömer şanlı on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct iTour_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
